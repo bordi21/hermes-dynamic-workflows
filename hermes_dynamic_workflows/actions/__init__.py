e0b276d7e834faba74aeb963d172984c10aa5a83
bordi21 <valibossdeboss@gmail.com>
2026-07-29 00:46:00 +0300

    [TASK-012] Export canonical reviewed workflow action

--- hermes_dynamic_workflows/actions/__init__.py
+++ hermes_dynamic_workflows/actions/__init__.py
@@ -4,11 +4,13 @@ from .execution import ReviewedTaskExecutionAction
 from .final_validation import FinalValidationAction
 from .planning import InitialPlanningAction, PlanningLimits
 from .reporting import TerminalReportAction
+from .workflow import ReviewedWorkflowAction
 
 __all__ = [
     "FinalValidationAction",
     "InitialPlanningAction",
     "PlanningLimits",
     "ReviewedTaskExecutionAction",
+    "ReviewedWorkflowAction",
     "TerminalReportAction",
 ]
